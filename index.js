80fde6b04486f8a3f86a77416fd1051cac3f14b0
gary <gary.white@lodgit.com>
2021-12-21 13:21:59 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,7 @@
+function switchItUp(number){
+    //Write your own Code!
+}
+
+console.log(switchItUp(1)); // Return "One"
+console.log(switchItUp(3)); // Return "Three"
+console.log(switchItUp(5)); // Return "Five"
